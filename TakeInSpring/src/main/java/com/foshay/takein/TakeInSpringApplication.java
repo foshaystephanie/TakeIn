cfe26c4ca2eaf3cfc71249e6a7a143471a3c1729
Stephanie Foshay <foshaystephanie@gmail.com>
2021-01-25 20:29:06 -0600

    create initial project

---
+++ TakeInSpring/src/main/java/com/foshay/takein/TakeInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.foshay.takein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeInSpringApplication.class, args);
+	}
+
+}
---
+++ TakeInSpring/src/test/java/com/foshay/takein/TakeInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foshay.takein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeInSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
